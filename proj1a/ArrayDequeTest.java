0d0bbaf3aabaec8c0acd916b14376fb335cd53be
jesushedd <jesushe99@gmail.com>
2024-07-10 10:28:09 -0500

    Array Deque tests: empty(); addFirst()

---
+++ proj1a/ArrayDequeTest.java
@@ -0,0 +1,53 @@
+public class ArrayDequeTest {
+
+    public static void testEmpty(){
+        ArrayDeque<Integer> dq = new ArrayDeque<Integer>();
+        boolean actual = dq.isEmpty();
+        boolean expected = true;
+        if (actual == expected){
+            System.out.println("Test empty passed!!");
+        }
+    }
+
+
+    public static void testAddFirst(){
+        ArrayDeque<String> dq = new ArrayDeque<String>();
+        //test first item
+        dq.addFirst("Hola Munda");
+        String actual = dq.get(0);
+        String expected = "Hola Munda";
+        if (expected.equals(actual)){
+            System.out.println("Test addFirst when first item added and get(0) Passed!!");
+        } else {
+            System.out.println("get(0) returned " + actual + " Expected: " +expected);
+        }
+
+        //test subsequent items at front when starting from index 0
+        dq.addFirst("Greca");
+        actual = dq.get(7);
+        expected = "Greca";
+        if (expected.equals(actual)){
+            System.out.println("Test addFirst when subsequents items added and get(i) Passed!!");
+        } else {
+            System.out.println("get(7) returned " + actual + " Expected: " +expected);
+        }
+
+        dq.addFirst("Safira");
+        actual = dq.get(6);
+        expected = "Safira";
+        if (expected.equals(actual)){
+            System.out.println("Test addFirst when subsequents items added and get(i) Passed!!");
+        } else {
+            System.out.println("get(6) returned " + actual + " Expected: " +expected);
+        }
+
+    }
+
+
+    public static void main(String[] args) {
+        testEmpty();
+        testAddFirst();
+
+
+    }
+}
