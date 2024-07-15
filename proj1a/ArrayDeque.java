0e06a88826cbe37799f21176b84756b50ea028f8
jesushedd <jesushe99@gmail.com>
2024-07-15 11:16:24 -0500

    add next and previous as methods for traversing array

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -27,8 +27,9 @@ public class ArrayDeque <T> {
         /*TODO
         //check if full
             //if full resize()*/
-        front = (front - 1 + items.length) % items.length;
+        front = prev(front);
         items[front] = item;
+        size++;
     }
 
     public void addLast(T item){
@@ -41,28 +42,49 @@ public class ArrayDeque <T> {
         //TODO
         //check if full
             //if full resize()
-        this.rear = (this.rear + 1) % this.items.length;
+        this.rear = next(rear);
         items[rear] = item;
         size ++;
 
     }
 
     private boolean isFull(){
-        return ((rear + 1 ) % items.length) == front;
+        return next(rear) == front;
     }
 
     public boolean isEmpty(){
         return front == -1;
     }
 
-    public void printDeque(){
 
+    /*Returns next index from index of current rear item */
+    private int next(int index){
+        return (index + 1) % items.length;
     }
 
+    /*Returns previous index from index of current front item*/
+    private int prev(int index){
+        return (index - 1 + items.length) % items.length;
+    }
+
+
+
+    //TODO
+    public void printDeque(){
+        int cursor = front;
+        while (cursor != next(rear)) {
+            System.out.println(items[cursor]);
+            cursor = next(cursor);
+        }
+    }
+
+    //TODO
     public T removeFirst(){
+
         return  (T) (new  Object());
     }
 
+    //TODO
     public T removeLast(){
         return (T) (new Object());
     }
