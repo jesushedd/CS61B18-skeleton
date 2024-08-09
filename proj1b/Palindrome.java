b81b2b41036062311b4ead0c7ad897e296ec0540
jesushedd <jesushe99@gmail.com>
2024-08-09 12:04:13 -0500

    general isPalindrome mmethod, and isPalindrome method using an interface

--- proj1b/Palindrome.java
+++ proj1b/Palindrome.java
@@ -1,3 +1,5 @@
+import java.io.FileWriter;
+
 public class Palindrome {
     public Deque<Character> wordToDeque (String word){
         Deque<Character> charDeque = new LinkedListDeque<>();
@@ -8,6 +10,43 @@ public class Palindrome {
 
         return charDeque;
     }
+
+    /*Returns true if a word is a palindrome, false otherwise*/
+    public boolean isPalindrome(String word){
+        Deque<Character> charList = wordToDeque(word);
+        String reversed = "";
+        while (true){
+            Character letter = charList.removeLast();
+            if (letter == null){
+                break;
+            }
+            reversed += letter;
+        }
+
+        return word.equals(reversed);
+    }
+
+    /*Returns true if a word is a palindrome by the CharacterComparator conditions given*/
+    public boolean isPalindrome(String word, CharacterComparator cc){
+        boolean isPalindrome = true;
+        Character leftChar;
+        Character rightChar;
+        Deque<Character> chars = wordToDeque(word);
+        while (chars.size() > 1){
+
+            leftChar = chars.removeFirst();
+            rightChar = chars.removeLast();
+
+            isPalindrome = cc.equalChars(leftChar, rightChar);
+            if (!isPalindrome){
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+
 
 
 
