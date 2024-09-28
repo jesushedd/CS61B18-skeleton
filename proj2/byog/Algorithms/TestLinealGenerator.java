5d550ae3e00e5a582fc7489f272bbeec05658d3d
jesushedd <jesushe99@gmail.com>
2024-09-28 13:52:33 -0500

    add test for LinealGenerator constructor

---
+++ proj2/byog/Algorithms/TestLinealGenerator.java
@@ -0,0 +1,32 @@
+package byog.Algorithms;
+
+import byog.TileEngine.TETile;
+import byog.TileEngine.Tileset;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Arrays;
+
+
+public class TestLinealGenerator {
+
+    TETile[][] testWorld = new TETile[80][30];
+
+
+    private static void fillVoids(TETile[][] w){
+
+        for(TETile[] Y : w){
+            Arrays.fill(Y, Tileset.NOTHING);
+        }
+    }
+
+    @Test
+    public void testMaxXY(){
+        fillVoids(testWorld);
+        LinealGenerator lineaAlgo = new LinealGenerator(testWorld);
+
+        Assert.assertEquals(79, lineaAlgo.getMAX_X());
+        Assert.assertEquals(29, lineaAlgo.getMAX_Y());
+
+    }
+}
