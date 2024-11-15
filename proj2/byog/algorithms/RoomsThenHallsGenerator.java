732e4087bcc2a5d75fa81decf4368255122c0579
jesushedd <jesushe99@gmail.com>
2024-11-14 21:46:50 -0500

    Complete desifition of Room Now includes drawing, of rooms wall. Also now it have a centroid

--- proj2/byog/algorithms/RoomsThenHallsGenerator.java
+++ proj2/byog/algorithms/RoomsThenHallsGenerator.java
@@ -6,6 +6,9 @@ import byog.Core.TileMatrixHelpers;
 import byog.TileEngine.Tileset;
 import byog.algorithms.position.Position;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Random;
 
 
@@ -15,9 +18,12 @@ public class RoomsThenHallsGenerator implements GenAlgorithm{
     private static class Room {
         private final Position botLeftCorner;
         private final Position topRightCorner;
+
+        private final Position centroid;
         private Room(Position botLeft, Position topRight ){
             botLeftCorner = botLeft;
             topRightCorner = topRight;
+            centroid = calculateCentroid(botLeft, topRight);
         }
 
         private int getWidth(){
@@ -26,6 +32,23 @@ public class RoomsThenHallsGenerator implements GenAlgorithm{
 
         private int getHeight(){
             return topRightCorner.getYyPosition() - botLeftCorner.getYyPosition();
+        }
+
+
+        private Position getCentroid(){
+            return centroid;
+        }
+
+        private Position calculateCentroid(Position botLeft, Position topRight){
+            int height = topRight.getYyPosition() - botLeft.getYyPosition();
+            int base = topRight.getXxPosition() - botLeft.getXxPosition();
+            return new Position(base / 2, height / 2);
+        }
+
+        private int areaFromCorners(Position botLeft, Position topRight){
+            int height = topRight.getYyPosition() - botLeft.getYyPosition();
+            int base = topRight.getXxPosition() - botLeft.getXxPosition();
+            return base * height;
         }
     }
     private final Random random;
@@ -36,47 +59,63 @@ public class RoomsThenHallsGenerator implements GenAlgorithm{
     private final int ROOM_MIN_LEN = 2;
     private final int ROOM_HEIGHT_MAX;
 
+    private final LinkedList<Room> listOfRooms = new LinkedList<>();
+    private int usedArea = 0;
+
 
 
     
-    private final float WIDTH_PROPORTION = (4F / 27);
-    private final float HEIGHT_PROPORTION = (3F/12);
+    private final float WIDTH_PROPORTION = (2F / 27);
+    private final float HEIGHT_PROPORTION = (1F/12);
     public RoomsThenHallsGenerator(TETile [][] inWorld, long seed){
 
         if (TileMatrixHelpers.anyTilesIsNull(inWorld)){
             throw new IllegalArgumentException("All tiles should be non null");
         }
+
+
         
         int heightOfWorld = inWorld[0].length;
         int widthOfWorld = inWorld.length;
+
+        if (heightOfWorld < 6 || widthOfWorld < 6){
+            throw new IllegalArgumentException("Minimum side of world is 6");
+        }
         random = new Random(seed);
         WORLD = inWorld;
         X_MAX = widthOfWorld - 1;
         Y_MAX = heightOfWorld - 1;
-        ROOM_WIDTH_MAX =  (int) (WIDTH_PROPORTION * widthOfWorld);
-        ROOM_HEIGHT_MAX = (int) (HEIGHT_PROPORTION * heightOfWorld);
+
+        int possibleWidth = (int) (WIDTH_PROPORTION * widthOfWorld);
+        int possibleHeight = (int) (HEIGHT_PROPORTION * heightOfWorld);
+        ROOM_WIDTH_MAX = Math.max(possibleWidth, ROOM_MIN_LEN);
+        ROOM_HEIGHT_MAX = Math.max(possibleHeight, ROOM_MIN_LEN);
+
+
+        if (X_MAX - ROOM_WIDTH_MAX <= 2 || Y_MAX - ROOM_WIDTH_MAX <= 2){
+            throw new IllegalArgumentException("Illegal aspect ratio");
+        }
     }
     @Override
     public void setTiles(){
-        Room room = getRandomRoom();
-        placeRoom(room);
+        placeRoom();
     }
 
     @Override
     public int getUsedArea() {
-        return 0;
+        return usedArea;
     }
 
     private Room getRandomRoom(){
         //create position x, y at top left corner
-        int x = RandomUtils.uniform(random, X_MAX + 1);
-        int y = RandomUtils.uniform(random, Y_MAX+ 1);
+        int x = RandomUtils.uniform(random,ROOM_MIN_LEN ,X_MAX - ROOM_WIDTH_MAX );
+        int y = RandomUtils.uniform(random,ROOM_MIN_LEN,Y_MAX  - ROOM_WIDTH_MAX);
 
         Position botLeftCorner = new Position(x, y);
         //getting a valid top right corner position from left corner
         Position topRightCorner = null;
         while (!isWithinBounds(topRightCorner)){
-            topRightCorner = randomBotCorner(botLeftCorner);
+            topRightCorner = randomTopCorner(botLeftCorner);
         }
         return new Room(botLeftCorner, topRightCorner);
     }
@@ -85,14 +124,14 @@ public class RoomsThenHallsGenerator implements GenAlgorithm{
         if (pos == null){
             return false;
         }
-        return (pos.getXxPosition() <= X_MAX && pos.getYyPosition() <= Y_MAX);
+        return ((pos.getXxPosition() <= X_MAX) && (pos.getYyPosition() <= Y_MAX));
     }
 
-    private Position randomBotCorner(Position botCorner){
+    private Position randomTopCorner(Position botCorner){
         //random width
-        int width = RandomUtils.uniform(random,ROOM_MIN_LEN, ROOM_WIDTH_MAX + 1);
+        int width = RandomUtils.uniform(random,ROOM_MIN_LEN, ROOM_WIDTH_MAX +1);
         //random height
-        int height = RandomUtils.uniform(random, ROOM_MIN_LEN, ROOM_HEIGHT_MAX + 1);
+        int height = RandomUtils.uniform(random,ROOM_MIN_LEN, ROOM_HEIGHT_MAX + 1);
 
         return new Position(botCorner.getXxPosition() + width , botCorner.getYyPosition() + height);
     }
@@ -100,17 +139,79 @@ public class RoomsThenHallsGenerator implements GenAlgorithm{
 
 
 
-    private void placeRoom(Room roomToPlace){
+    private void placeRoom(){
+        //crate random room
+        Room roomToPlace = getRandomRoom();
+        listOfRooms.add(roomToPlace);
+        placeRoomInterior(roomToPlace);
+        placeRoomWalls(roomToPlace);
+
+
+
+
+    }
+    private void placeRoomInterior(Room room){
         // Get starting coordinates
-        int xStart = roomToPlace.botLeftCorner.getXxPosition();
-        int yStart = roomToPlace.botLeftCorner.getYyPosition();
+        int xStart = room.botLeftCorner.getXxPosition();
+        int yStart = room.botLeftCorner.getYyPosition();
         //fill tiles
-        for (int y = yStart ; y <= yStart + roomToPlace.getHeight(); y++) {
-            for (int x = xStart; x < xStart + roomToPlace.getWidth()  ; x++) {
-                WORLD[x][y] = Tileset.FLOOR;
+        for (int y = yStart ; y <= yStart + room.getHeight(); y++) {
+            for (int x = xStart; x <= xStart + room.getWidth()  ; x++) {
+                if (tilesIsNothing(x,y)){
+                    usedArea++;
+                }
+                WORLD[x][y] = Tileset.GRASS;
             }
         }
+
+    }
+
+    private void placeRoomWalls(Room room){
+        //place vertical walls
+        //left
+        int x = room.botLeftCorner.getXxPosition() - 1;
+        int startY = room.botLeftCorner.getYyPosition() - 1;
+        int endY = room.topRightCorner.getYyPosition() + 1;
+        for (int i = startY; i <= endY ; i++) {
+            if (tilesIsNothing(x, i)) {
+                WORLD[x][i] = Tileset.WALL;
+            }
         }
+
+        //right
+        x = room.topRightCorner.getXxPosition() + 1;
+        for(int i = startY; i <= endY ; i++) {
+            if (tilesIsNothing(x, i)) {
+                WORLD[x][i] = Tileset.WALL;
+            }
+        }
+        //place horizontal walls
+        //bottom
+        int y = room.botLeftCorner.getYyPosition() - 1;
+        int startX = room.botLeftCorner.getXxPosition();
+        int endX = room.topRightCorner.getXxPosition();
+
+        for(int i = startX; i <= endX ; i++) {
+            if (tilesIsNothing(i, y)) {
+                WORLD[i][y] = Tileset.WALL;
+            }
+        }
+        //top
+        y = room.topRightCorner.getYyPosition() + 1;
+        for(int i = startX; i <= endX ; i++) {
+            if (tilesIsNothing(i, y)) {
+                WORLD[i][y] = Tileset.WALL;
+            }
+        }
+
+
+
+    }
+    private boolean tilesIsNothing(int x, int y){
+        return WORLD[x][y] == Tileset.NOTHING;
+    }
+
+
 
 
     }
