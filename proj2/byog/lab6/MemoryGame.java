aae0ace910c5e1c0204c267ca31c5195f2517c00
jesushedd <jesushe99@gmail.com>
2025-01-04 14:18:10 -0500

    Create Randomb objet from seed arg

--- proj2/byog/lab6/MemoryGame.java
+++ proj2/byog/lab6/MemoryGame.java
@@ -25,11 +25,12 @@ public class MemoryGame {
         }
 
         int seed = Integer.parseInt(args[0]);
-        MemoryGame game = new MemoryGame(40, 40);
+        Random rand = new Random(seed);
+        MemoryGame game = new MemoryGame(40, 40, rand);
         game.startGame();
     }
 
-    public MemoryGame(int width, int height) {
+    public MemoryGame(int width, int height, Random rand) {
         /* Sets up StdDraw so that it has a width by height grid of 16 by 16 squares as its canvas
          * Also sets up the scale so the top left is (0,0) and the bottom right is (width, height)
          */
