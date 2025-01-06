2b06e41d2917a18940e04c88871c90a06bedb1fa
jesushedd <jesushe99@gmail.com>
2025-01-06 12:48:54 -0500

    Set sprite of player

--- proj2/byog/TileEngine/Tileset.java
+++ proj2/byog/TileEngine/Tileset.java
@@ -17,7 +17,7 @@ import java.awt.Color;
  */
 
 public class Tileset {
-    public static final TETile PLAYER = new TETile('@', Color.white, Color.black, "player");
+    public static final TETile PLAYER = new TETile('@', Color.white, Color.black, "player", "I:\\OSSU\\programming\\CS61B_Spring_2018_java\\skeleton-sp18\\proj2\\byog\\TileEngine\\player.png");
     public static final TETile WALL = new TETile('#', new Color(216, 128, 128), Color.darkGray,
             "wall");
     public static final TETile FLOOR = new TETile('·', new Color(128, 192, 128), Color.black,
